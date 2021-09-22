7f95aca9ae7474814c9dfae57bf83fdec09f56f7
Raquibul Islam <shubha360@outlook.com>
2021-09-22 18:30:36 +0600

    javadocs added

--- README.md
+++
@@ -1,2 +0,0 @@
-# Project-ConnectN
-The Java-made console version of "Connect 4" game.
---
+++ javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.dselent.solution","l":"ConnectN"}]
